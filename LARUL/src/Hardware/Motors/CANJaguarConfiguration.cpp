63f568b4e353147eabc35a2fc44c4459a5772b10
Ian <ianproth@gmail.com>
2015-01-14 16:04:57 -0800

    CANJag config

--- LARUL/src/Hardware/Motors/CANJaguarConfiguration.cpp
+++ LARUL/src/Hardware/Motors/CANJaguarConfiguration.cpp
@@ -1,3 +1,183 @@
 #include "CANJaguarConfiguration.h"
+#include <limits>
 
+CANJaguarConfiguration :: CANJaguarConfiguration ( CANSpeedController :: ControlMode CtlMode):
+NeutralMode ( CANSpeedController :: kNeutralMode_Jumper),
+LimitMode  ( CANSpeedController :: kLimitMode_SwitchInputsOnly),
+ForwardLimitPosition ( std::numeric_limits<int>::max() ),
+ReverseLimitPosition (  std::numeric_limits<int>::max() ),
+MaxOutputVoltage ( 15.0 ),
+FaultTime ( 0.51f ),
+VoltageRampRate ( 1000 ),
+ControlMode ( CtlMode ),
+Feedback ( kFeedbackType_None ),
+Counts ( ),
+P ( ),
+I ( ),
+D ( )
+
+{
+
+}
+
+CANJaguarConfiguration :: ~CANJaguarConfiguration ( ) 
+{
+
+}
+
+void CANJaguarConfiguration :: DissableFeedback ( ) 
+{
+	Feedback = kFeedbackType_None;
+}
+
+void CANJaguarConfiguration :: SetConfPID ( double & pConf, double & iConf, double & dConf )
+{
+	P = pConf;
+	I = iConf;
+	D = dConf;
+}
+
+void CANJaguarConfiguration :: SetEncoderFeedback ( double pConf, double iConf, double dConf, uint16_t codesPerRev)
+{
+	SetConfPID ( pConf, iConf, dConf);
+	Feedback = kFeedbackType_Encoder;
+	Counts = codesPerRev;
+}
+
+void CANJaguarConfiguration :: SetPotFeedBack ( double pConf, double iConf, double dConf, uint16_t turns)
+{
+	SetConfPID ( pConf, iConf, dConf);
+	Feedback = kFeedbackType_Potentiometer;
+	Counts = turns;
+}
+
+void CANJaguarConfiguration :: SetQuadEncoderFeedback ( double pConf, double iConf, double dConf, uint16_t codesPerRev)
+{
+	SetConfPID ( pConf, iConf, dConf);
+	Feedback = kFeedbackType_QuadEncoder;
+	Counts = codesPerRev;
+}
+
+void CANJaguarConfiguration :: SetLimits ( CANSpeedController :: LimitMode LimitType, double ConfigForwardLimit, double ConfigReverseLimit )
+{
+	LimitMode = LimitType;
+	ForwardLimitPosition = ConfigForwardLimit;
+	ReverseLimitPosition = ConfigReverseLimit;
+}
+
+void CANJaguarConfiguration :: SetFaultTime ( double ConfigFaultTime )
+{
+	FaultTime = ConfigFaultTime;
+}
+
+void CANJaguarConfiguration :: SetMaxOutputVoltage ( double ConfigVoltage )
+{
+	MaxOutputVoltage = ConfigVoltage;
+}
+
+
+void CANJaguarConfiguration :: ConfigJag (  CANJaguar & Jag )
+{
+	Jag.SetVoltageRampRate ( VoltageRampRate );
+	Jag.ConfigFaultTime ( FaultTime );
+	Jag.ConfigNeutralMode ( NeutralMode );
+	Jag.ConfigLimitMode ( LimitMode );
+	Jag.ConfigForwardLimit ( ForwardLimitPosition );
+	Jag.ConfigReverseLimit ( ReverseLimitPosition );
+	Jag.ConfigMaxOutputVoltage ( MaxOutputVoltage );
+	
+	switch ( Feedback )
+	{
+
+	case kFeedbackType_None:
+
+		switch ( ControlMode )
+		{
+
+		case CANSpeedController :: kPercentVbus:
+			Jag.SetPercentMode ( );
+			break;
+
+		case CANSpeedController :: kCurrent:
+			Jag.SetCurrentMode ( P ,I ,D );
+			break;
+
+		case CANSpeedController :: kVoltage:
+			Jag.SetVoltageMode ( );
+			break;
+
+		default:
+
+			break;
+
+		}
+		break;
+
+	case kFeedbackType_Encoder:
+
+		switch ( ControlMode )
+		{
+			case CANSpeedController :: kPercentVbus:
+				Jag.SetPercentMode ( CANJaguar :: Encoder, Counts );
+				break;
+
+			case CANSpeedController :: kCurrent:
+				Jag.SetCurrentMode ( CANJaguar :: Encoder, Counts, P, I, D );
+				break;
+
+			case CANSpeedController :: kSpeed:
+				Jag.SetSpeedMode ( CANJaguar :: Encoder, Counts, P, I, D );
+				break;
+
+			case CANSpeedController :: kVoltage:
+				Jag.SetSpeedMode ( CANJaguar :: Encoder, Counts, P, I, D );
+				break;
+			default:
+				break;
+		}
+		break;
+	case kFeedbackType_QuadEncoder:
+
+		switch ( ControlMode )
+		{
+			case CANSpeedController :: kPercentVbus:
+				Jag.SetPercentMode ( CANJaguar :: QuadEncoder, Counts );
+				break;
+			case CANSpeedController :: kCurrent:
+				Jag.SetCurrentMode ( CANJaguar :: QuadEncoder, Counts, P, I, D );
+				break;
+			case CANSpeedController :: kSpeed:
+				Jag.SetSpeedMode ( CANJaguar :: QuadEncoder, Counts, P, I, D );
+				break;
+			case CANSpeedController :: kPosition:
+				Jag.SetPositionMode ( CANJaguar :: QuadEncoder, Counts, P, I, D );
+				break;
+			case CANSpeedController :: kVoltage:
+				Jag.SetVoltageMode ( CANJaguar :: QuadEncoder, Counts);
+				break;
+			default:
+				break;
+		}
+		break;
+	case kFeedbackType_Potentiometer:
+		switch ( ControlMode )
+		{
+			case CANSpeedController :: kPercentVbus:
+				Jag.SetPercentMode ( CANJaguar :: Potentiometer);
+				break;
+			case CANSpeedController :: kCurrent:
+				Jag.SetCurrentMode ( CANJaguar :: Potentiometer, P, I, D );
+				break;
+			case CANSpeedController :: kPosition:
+				Jag.SetPositionMode ( CANJaguar :: Potentiometer, P, I, D );
+				break;
+			case CANSpeedController :: kVoltage:
+				Jag.SetVoltageMode ( CANJaguar :: Potentiometer);
+				break;
+			default:
+				break;
+		}
+		break;
+	}
+}
 
--- LARUL/src/Hardware/Motors/CANJaguarConfiguration.h
+++ LARUL/src/Hardware/Motors/CANJaguarConfiguration.h
@@ -5,41 +5,52 @@
 
 class CANJaguarConfiguration
 {
+	
+
+
+
+	
 public:
-	
-private:
-	
-	typedef enum
+	enum FeedbackType
 	{
-		
+		kFeedbackType_None,
 		kFeedbackType_QuadEncoder,
 		kFeedbackType_Encoder,
-		kFeedbackType,Potentiometer
-		
-	} FeedbackType;
-	
-	CANSpeedController :: ControlMode Mode;
-	FeedbackType Feedback;
-	
-	union
-	{
-		
-		struct
-		{
-			
-			
-			
-		} EncoderConf;
-		
-		struct
-		{
-			
-			
-			
-		} PotConf;
+		kFeedbackType_Potentiometer,
 		
 	};
+
+
+
+	CANJaguarConfiguration ( CANSpeedController :: ControlMode CtlMode );
+	~CANJaguarConfiguration ( );
 	
+	void DissableFeedback ( );
+	void SetConfPID ( double & pConf, double & iConf, double & dConf );
+	void SetEncoderFeedback ( double pConf, double iConf, double dConf, uint16_t codesPerRev);
+	void SetPotFeedBack ( double pConf, double iConf, double dConf, uint16_t turns);
+	void SetQuadEncoderFeedback ( double pConf, double iConf, double dConf, uint16_t codesPerRev);
+
+	void SetLimits ( CANSpeedController :: LimitMode LimitType, double ConfigForwardLimit, double ConfigReverseLimit );
+	void SetFaultTime ( double ConfigFaultTime );
+	void SetMaxOutputVoltage ( double ConfigVoltage );
+	void SetVoltageRampRate ( double Rate );
+
+	void ConfigJag (  CANJaguar & Jag );
+
+private:
+	CANSpeedController :: NeutralMode NeutralMode;
+	CANSpeedController :: LimitMode LimitMode;
+	double ForwardLimitPosition;
+	double ReverseLimitPosition;
+	double MaxOutputVoltage;
+	double FaultTime;
+	double VoltageRampRate;
+	
+	CANSpeedController :: ControlMode ControlMode;
+	FeedbackType Feedback;
+	double P, I, D;
+	uint16_t Counts;
 };
 
 

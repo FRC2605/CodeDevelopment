d435598bb7d8788b7d07d34651f041c07533c376
Ian <ianproth@gmail.com>
2015-02-04 11:08:22 -0800

    For Clarity

--- IUL/src/Input/SmartJoystick.cpp
+++ IUL/src/Input/SmartJoystick.cpp
@@ -1,8 +1,12 @@
 #include "SmartJoystick.h"
-#include <cmath>
 
-SmartJoystick :: SmartJoystick ( int JoystickNumber, double Deadzone):
-	Joystick ( JoystickNumber ),
+/*
+* Copyright (C) 2015 Ian Roth
+* WheelFRC Team Sehome Semonsters 2605
+*/
+
+SmartJoystick :: SmartJoystick ( int JoystickID, double Deadzone):
+	Joystick ( JoystickID ),
 	AxisDeadzone ( Deadzone )
 {
 };
@@ -14,8 +18,6 @@ double SmartJoystick :: GetXAxis ( bool WithDeadzone, double Deadzone)
 
 	if ( WithDeadzone )
 	{
-		if ( Deadzone == 0.0 )
-			Deadzone = AxisDeadzone;
 		CalcDeadzone ( X , Deadzone);
 	}
 	return X;
@@ -28,8 +30,6 @@ double SmartJoystick :: GetYAxis ( bool WithDeadzone, double Deadzone)
 
 	if ( WithDeadzone )
 	{
-		if ( Deadzone == 0.0 )
-			Deadzone = AxisDeadzone;
 		CalcDeadzone ( Y , Deadzone);
 	}
 	return Y;
@@ -42,26 +42,29 @@ double SmartJoystick :: GetZAxis ( bool WithDeadzone, double Deadzone)
 
 	if ( WithDeadzone )
 	{
-		if ( Deadzone == 0.0 )
-			Deadzone = AxisDeadzone;
 		CalcDeadzone ( Z , Deadzone);
 	}
 	return Z;
 };
 
-bool SmartJoystick :: GetButtonState ( uint32_t ButtonNumber )
+bool SmartJoystick :: GetButtonState ( uint32_t ButtonID )
 {
 	double State;
-	State = this -> GetRawButton ( ButtonNumber );
+	State = this -> GetRawButton ( ButtonID );
 
 	if ( State == 0 )
+
 		return true;
+
 	else
+
 		return false;
 };
 
-void SmartJoystick :: CalcDeadzone ( float & AxisIn, double & Deadzone )
+void SmartJoystick :: CalcDeadzone ( float & Value, double & Deadzone )
 {
-	if ( abs ( AxisIn ) <= ( Deadzone / 100 ) )
-		AxisIn = 0;
+	if ( Deadzone == 0.0 )
+		Deadzone = AxisDeadzone;
+	if ( abs ( Value ) <= ( Deadzone / 100 ) )
+		Value = 0;
 };
--- IUL/src/Input/SmartJoystick.h
+++ IUL/src/Input/SmartJoystick.h
@@ -1,21 +1,27 @@
-#ifndef SMARTJOYSTICK_H_
-#define SMARTJOYSTICK_H_
+#ifndef IUL_SMARTJOYSTICK_H_
+#define IUL_SMARTJOYSTICK_H_
 
-#include "WPILib.h"
+#include <WPILib.h>
+#include <cmath>
 
 class SmartJoystick : public Joystick
 {
 
 public:
-	SmartJoystick ( int JoystickNumber,  double Deadzone = 0.0 );
+
+	SmartJoystick ( int JoystickID,  double Deadzone = 0.0 );
 
 	double GetXAxis ( bool WithDeadzone = true, double Deadzone = 0.0 );
 	double GetYAxis ( bool WithDeadzone = true, double Deadzone = 0.0 );
 	double GetZAxis ( bool WithDeadzone = true, double Deadzone = 0.0 );
-	bool GetButtonState ( uint32_t ButtonNumber );
+
+	bool GetButtonState ( uint32_t ButtonID );
+
 private:
+
 	double AxisDeadzone;
-	void CalcDeadzone ( float & AxisIn, double & Deadzone );
+	void CalcDeadzone ( float & Value, double & Deadzone );
+
 };
 
 

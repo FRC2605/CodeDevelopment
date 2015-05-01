adb2d20dfb65f0d64feb73461781725a4484625c
Liam <Liam>
2015-04-30 18:40:13 -0700

    Added some documentation.

--- LARUL/src/Behaviors/BehaviorController.h
+++ LARUL/src/Behaviors/BehaviorController.h
@@ -8,6 +8,40 @@
 
 #include "../Util/Vector.h"
 
+/* BehaviorController: A controller object to manage the simultaneous execution of
+ * behaviors. ( Behaviors being any instance of a class that implements the IBehavior
+ * interface. )
+ *
+ * Methods:
+ *
+ * 	void AddBehavior ( IBehavior * NewBehavior, const char * Name )
+ * 	---------------------------------------------------------------------------------
+ * 	Adds a behavior to the internal map with a specific name. This name becomes the
+ * 	unique identifier for this behavior, and is used to manipulate it's state through
+ * 	the controller.
+ *
+ *	void RemoveBehavior ( const char * Name )
+ * 	---------------------------------------------------------------------------------
+ * 	Removes the behavior with the specified name. If it does not exist, nothing
+ * 	happens. Any running behavior removed will first be stopped.
+ *
+ *	void StartBehavior ( const char * Name )
+ * 	---------------------------------------------------------------------------------
+ * 	Starts a behavior with the given name. If none with this name exists, nothing
+ * 	happens. A behavior which is already running will not be re-started.
+ *
+ *	void StopBehavior ( const char * Name )
+ * 	---------------------------------------------------------------------------------
+ * 	Stops a behavior with the given name. If none with this name exists, nothing
+ * 	happens. A behavior which is not running will not be re-stopped.
+ *
+ *	void Update ()
+ * 	---------------------------------------------------------------------------------
+ * 	Updates the controller's active behaviors. This should be called periodically so
+ * 	long as any of the controller's behaviors are expected to be active.
+ *
+ */
+
 class BehaviorController
 {
 public:
